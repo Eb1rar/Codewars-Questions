de6da0b7e6a0d9a1471125d0cf291779f4367dd4
ebrar <ebrkdmr1@gmail.com>
2023-11-11 17:48:34 +0300

    continue

--- app.js
+++ app.js
@@ -13,3 +13,16 @@
 //   return arr.reduce((a, b) => a + (b > 0 ? b : 0), 0);
 // };
 // console.log(positiveSum([1, -4, 5, 65]));
+
+//! Square(n) Sum
+// const squareSum = (numbers)=>{
+//   return numbers.reduce((a,b) => a+(b**2),0)
+//   }
+//   console.log(squareSum([1, 2, 2]))
+
+//! Sum of two lowest positive integers
+// const sumTwoSmallestNumbers = (arr) => {
+//   arr.sort((a, b) => a - b);
+//   return (sum = arr[0] + arr[1]);
+// };
+// console.log(sumTwoSmallestNumbers([19, 5, 42, 2, 77]));
