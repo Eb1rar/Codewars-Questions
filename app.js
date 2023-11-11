1baa907a33ee656a235bb5cfaaef880a280399d5
ebrar <ebrkdmr1@gmail.com>
2023-11-11 16:50:01 +0300

    Codewars

---
+++ app.js
@@ -0,0 +1,15 @@
+// //!Sum of Positive
+
+// const positiveSum = () => {
+//   let numbers = [1, 0, -4, 12];
+//   let sum = numbers.reduce((a, b) => a + (b > 0 ? b : 0));
+//   return sum;
+// };
+// console.log(positiveSum());
+
+//* OR
+
+// const positiveSum = (arr) => {
+//   return arr.reduce((a, b) => a + (b > 0 ? b : 0), 0);
+// };
+// console.log(positiveSum([1, -4, 5, 65]));
